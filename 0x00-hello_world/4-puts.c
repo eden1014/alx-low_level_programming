4f3df313c4532e19dc25cc2d5cd8f8693387b1bd
eden1014 <hailueden14@gmail.com>
2022-04-07 14:08:48 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /* *
- * *Description:main-Prints out a string to stdout.
- * Return:0 if success.
- * */
+ * main - entry point
+ * Return: Always 0 (succes)
+ */
 int main ( void )
 {
 	puts ( " \" Programmming is like building a multilingual puzzle " );

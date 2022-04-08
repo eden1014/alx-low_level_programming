be0420366cffc26162efe94b351df9476ab39bd9
eden1014 <hailueden14@gmail.com>
2022-04-08 12:00:19 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,11 +3,12 @@
 /**
  * main - prints all possible combinations of two different digits,
  *         in ascending order, separated by a comma followed by a space.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-int digit1. digit2;
+int digit1, digit2;
 
 for (digit1 = 0; digit1 < 9; digit1++)
 {

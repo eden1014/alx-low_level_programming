273142493902041bc9d9be9b05ee6982afe4de00
eden1014 <hailueden14@gmail.com>
2022-04-14 06:42:28 -0700

    2

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+ * print numbers - Prints the number zero to nine
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_numbers(void)
+{
+
+int i;
+
+i = 0;
+
+for (i = 0; i < 10; i++)
+
+_putchar(i + '0');
+
+_putchar ('\n');
+}
